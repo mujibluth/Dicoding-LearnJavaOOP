6d81dab39aa27618cd94b43ec16892cff4b2313a
5heeL7eR <abdulmujiburrohmanluthfi123@gmail.com>
2024-05-21 17:13:14 +0700

    feat(project): add matery about Perulangan

---
+++ Perulangan/src/com/dicoding/javafundamental/perulangan/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int value = 1;
+        do {
+            System.out.println("Angka : " + value);
+            value++;
+        } while (value <= 10);
+    }
+}
---
+++ Perulangan/src/com/dicoding/javafundamental/perulangan/Whille.java
@@ -0,0 +1,10 @@
+public class Whille {
+    public static void main(String[] args) {
+        int value = 1;
+        while (value <= 10) {
+            System.out.print("Angka : " + value);
+            value++;
+            System.out.print("\n");
+        }
+    }
+}
