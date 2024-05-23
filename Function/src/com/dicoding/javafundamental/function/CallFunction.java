105705469ba75335aa9401519880589a22a192ee
5heeL7eR <abdulmujiburrohmanluthfi123@gmail.com>
2024-05-23 23:56:01 +0700

    feat(project): add matery about Function

---
+++ Function/src/com/dicoding/javafundamental/function/CallFunction.java
@@ -0,0 +1,18 @@
+public class CallFunction {
+    public static void main(String[] args) {
+        // memanggil fungsi 
+        cobaFungsi();
+    }
+
+    // modifier returnType nameOfFunction(parameters) {
+
+    // }
+    // modifier : menunjukkan sifat yang dimiliki pada suatu fungsi seperti public, private, protected.
+    // returnType : merupakan nilai balik yang diberikan oleh fungsi. Apabila fungsi tidak memiliki nilai balik maka menggunakan void.
+    // nameOfFunction : adalah nama dari sebuah fungsi.
+    // parameters : bersifat opsional, Suatu fungsi dapat mempunyai banyak parameters atau pun tidak sama sekali.
+
+    public static void cobaFungsi() {
+        System.out.println("ini merupakan bagian dari fungsi");
+    }
+}
